64ba415167611e6b47e51a95a117f86fc86a3cf8
Кирилл <kirill.denisenko.01@gmail.com>
2023-10-30 14:25:58 +0200

    Решил поиграть с вебпаком

--- task 6/webpack.config.dev.js
+++ task 6/webpack.config.dev.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
+    mode:'development',
     entry: './scripts/index.js',
+    watch: true,
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
---
+++ task 6/webpack.config.prod.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: './scripts/index.js',
+    watch: false,
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader', // Add Babel loader if needed
+            },
+        ],
+    },
+};
