d604f355c43b5898b75230cdc7cd43ce690bc2f0
Кирилл <kirill.denisenko.01@gmail.com>
2023-11-07 17:02:49 +0200

    Расширил класс Barrier

--- task7/scripts/barrier.js
+++ task7/scripts/barrier.js
@@ -1,8 +1,16 @@
 class Barrier {
-    constructor(hp) {
-        this.hp=hp
-    }
+  constructor(hp) {
+    this.hp = hp;
+  }
 
+  static createBarrier(positionX, positionY) {
+    const barrier = new Barrier(1);
+
+    return {
+      barrier,
+      position: [positionX, positionY],
+    };
+  }
 }
 
-export { Barrier }
+export { Barrier };
