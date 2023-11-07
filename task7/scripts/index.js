670ce677c043af555330deb30b9c7777f7d081bb
Кирилл <kirill.denisenko.01@gmail.com>
2023-11-07 17:04:12 +0200

    Изменил отрисовку Элементов

--- task7/scripts/index.js
+++ task7/scripts/index.js
@@ -1,86 +1,141 @@
-import {Game} from "./game";
+import { Game } from './game';
 
-let game = new Game()
+let game;
 
-function startGame(){
-    document.getElementById('hi-scope').innerText = game.hiScore;
-    document.getElementById('count-level').innerText = game.level;
-    document.getElementById('count-lives').innerText = game.getPlayerLives();
-    game.gameStart()
-
-    setInterval(()=>{
-        updateDisplay()
-    },500)
+function startGame() {
+  game = new Game();
+  game.gameStart();
+  setInterval(() => {
+    updateDisplay();
+  }, 500);
 }
 
+function renderEnemy(positionY, positionX) {
+  const battleField = document.getElementById('battlefield').children[0];
 
+  if (game.battlefield[positionY][positionX]?.enemy.level === 1) {
 
-function updateDisplay(){
-    const battleField = document.getElementById('battlefield').children[0]
-    for (let y = 0; y < 22; y++) {
-        for(let x = 0; x < 35; x++){
-            if(game.battlefield[y][x]?.enemy){
+    battleField.children[positionY].children[positionX].style.backgroundColor = '#ADFF2F';
 
-                battleField.children[y].children[x].style.backgroundColor='orange'
+  }
 
-            } else if(game.battlefield[y][x]?.barrier){
+  if (game.battlefield[positionY][positionX]?.enemy.level === 2) {
 
-                battleField.children[y].children[x].style.backgroundColor='red'
+    battleField.children[positionY].children[positionX].style.backgroundColor = '#FF69B4';
 
-            } else if(game.battlefield[y][x]?.player){
+  }
 
-                battleField.children[y].children[x].style.backgroundColor='yellow'
+  if (game.battlefield[positionY][positionX]?.enemy.level === 3) {
 
-            } else if(game.battlefield[y][x]?.bullet){
+    battleField.children[positionY].children[positionX].style.backgroundColor = '#FFD700';
 
-                battleField.children[y].children[x].style.backgroundColor='gray'
+  }
 
-            } else {
-                battleField.children[y].children[x].style.backgroundColor='green'
-            }
-        }
+  if (game.battlefield[positionY][positionX]?.enemy.level === 4) {
+
+    battleField.children[positionY].children[positionX].style.backgroundColor = '#20B2AA';
+
+  }
+
+  if (game.battlefield[positionY][positionX]?.enemy.level === 5) {
+
+    battleField.children[positionY].children[positionX].style.backgroundColor = '#4682B4';
+
+  }
+}
+
+function updateDisplay() {
+  document.getElementById('hi-scope').innerText = game.hiScore;
+  document.getElementById('count-level').innerText = game.level;
+  document.getElementById('count-lives').innerText = game.getPlayerLives();
+  const battleField = document.getElementById('battlefield').children[0];
+
+  if (game.win) {
+    winner();
+  }
+
+  if (game.gameOver) {
+    gameOver();
+  }
+  for (let y = 0; y < 22; y++) {
+    for (let x = 0; x < 35; x++) {
+      if (game.battlefield[y][x]?.enemy) {
+
+        renderEnemy(y, x)
+
+      } else if (game.battlefield[y][x]?.barrier) {
+
+        battleField.children[y].children[x].style.backgroundColor = 'red';
+
+      } else if (game.battlefield[y][x]?.player) {
+
+        battleField.children[y].children[x].style.backgroundColor = 'yellow';
+
+      } else if (game.battlefield[y][x]?.playerBullet) {
+
+        battleField.children[y].children[x].style.backgroundColor = 'gray';
+
+      } else if (game.battlefield[y][x]?.abilityLive) {
+
+        battleField.children[y].children[x].style.backgroundColor = '#800000';
+
+      } else if (game.battlefield[y][x]?.enemyBullet) {
+
+        battleField.children[y].children[x].style.backgroundColor = '#696969';
+
+      } else {
+
+        battleField.children[y].children[x].style.backgroundColor = 'green';
+
+      }
     }
+  }
 }
 
 document.addEventListener('keydown', (event) => {
-    if ( event.key === 'ArrowLeft') {
-        game.movePlayerLeft()
-    } else if( event.key === " "){
-        game.createBulletPlayer()
-    } else if ( event.key === 'ArrowRight' )  {
-        game.movePlayerRight()
-    }
-    updateDisplay()
+  event.preventDefault();
+  if (event.key === 'ArrowLeft') {
+    game.movePlayerLeftOnBattlefield();
+  } else if (event.key === ' ' && event.target === document.body) {
+    game.createBulletPlayerOnBattlefield();
+  } else if (event.key === 'ArrowRight') {
+    game.movePlayerRightOnBattlefield();
+  }
+  updateDisplay();
 });
 
+document.getElementById('start-button').addEventListener('click', () => {
+  document.getElementsByClassName('start-page')[0].style.display = 'none';
+  document.getElementsByClassName('game-page')[0].style.display = 'block';
 
-document.getElementById('start-button').addEventListener('click',()=>{
-    document.getElementsByClassName('start-page')[0].style.display='none'
-    document.getElementsByClassName('game-page')[0].style.display='block'
+  startGame();
+});
 
-    startGame()
-})
-document.getElementById('winner-page-start-button').addEventListener('click',()=>{
-    document.getElementsByClassName('game-page')[0].style.display='block'
-    document.getElementsByClassName('winner-page')[0].style.display='none'
+document
+  .getElementById('winner-page-start-button')
+  .addEventListener('click', () => {
+    document.getElementsByClassName('game-page')[0].style.display = 'block';
+    document.getElementsByClassName('winner-page')[0].style.display = 'none';
 
-    startGame()
-})
-document.getElementById('game-over-start-button').addEventListener('click',()=>{
-    document.getElementsByClassName('game-page')[0].style.display='block'
-    document.getElementsByClassName('game-over-page')[0].style.display='none'
+    startGame();
+  });
 
-    startGame()
-})
+document
+  .getElementById('game-over-start-button')
+  .addEventListener('click', () => {
+    document.getElementsByClassName('game-page')[0].style.display = 'block';
+    document.getElementsByClassName('game-over-page')[0].style.display = 'none';
 
-function winner(){
-    document.getElementsByClassName('game-page')[0].style.display='none'
-    document.getElementsByClassName('winner-page')[0].style.display='block'
+    startGame();
+  });
 
+function winner() {
+  document.getElementsByClassName('game-page')[0].style.display = 'none';
+  document.getElementsByClassName('winner-page')[0].style.display = 'block';
+  document.getElementById('winner-score-log').innerText = game.hiScore;
 }
 
-function gameOver(){
-    document.getElementsByClassName('game-page')[0].style.display='none'
-    document.getElementsByClassName('game-over-page')[0].style.display='block'
-    game.gameOver()
+function gameOver() {
+  document.getElementsByClassName('game-page')[0].style.display = 'none';
+  document.getElementsByClassName('game-over-page')[0].style.display = 'block';
 }
