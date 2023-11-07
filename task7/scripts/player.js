b333798957d26be0fc30f575bba078d7277e10fc
Кирилл <kirill.denisenko.01@gmail.com>
2023-11-07 17:01:51 +0200

    Расширил класс Player

--- task7/scripts/player.js
+++ task7/scripts/player.js
@@ -1,13 +1,56 @@
 class Player {
-    #lives
-    constructor(lives= 3) {
-        this.#lives = lives
-    }
+  #lives;
+  #position;
+  constructor(position = []) {
+    this.#lives = 3;
+    this.#position = position;
+  }
 
-    get lives(){
-        return this.#lives
+  addLive() {
+    if (this.#lives <= 4) {
+      this.#lives += 1;
     }
+  }
 
+  damageLives() {
+    if (this.#lives >= 0) {
+      this.#lives -= 1;
+    }
+  }
+
+  static movePlayerRight(game) {
+    let row = game.battlefield[21];
+    for (let x = row.length; x >= 0; x--) {
+      if (row[x]?.player) {
+        if (x < game.sizeX - 1) {
+          row[x + 1] = row[x];
+          row[x] = row[x - 1];
+          game.player.position[0] += 1;
+        }
+      }
+    }
+  }
+
+  static movePlayerLeft(game) {
+    let row = game.battlefield[21];
+    for (let x = 0; x < row.length; x++) {
+      if (row[x]?.player) {
+        if (x > 0) {
+          row[x - 1] = row[x];
+          row[x] = row[x + 1];
+          game.player.position[0] -= 1;
+        }
+      }
+    }
+  }
+
+  get lives() {
+    return this.#lives;
+  }
+
+  get position() {
+    return this.#position;
+  }
 }
 
-export {Player}
+export { Player };
