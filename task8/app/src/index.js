7bacadb266037e032fa656009b5a851f48af72dd
Кирилл <kirill.denisenko.01@gmail.com>
2023-11-13 12:57:04 +0200

    Обновление логики последовательности с роутами

--- task8/app/src/index.js
+++ task8/app/src/index.js
@@ -5,15 +5,13 @@ const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, '..', 'public', 'static')));
 
-//console.log(path.resolve(__dirname, '..', '..', 'app', 'public', 'static'))
-
-app.get('/', (req, res) => {
-    res.render(path.resolve(__dirname, '..', 'public', 'static', 'index.html'));
-});
-
 app.get('/ping',(req, res)=>{
     res.json({message:"pong"})
 })
+
+app.get('*', (req, res) => {
+    res.render(path.resolve(__dirname, '..', 'public', 'static', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
